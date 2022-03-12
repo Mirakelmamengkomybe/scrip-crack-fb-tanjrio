f752106228a9f28a4cf65551bbd40f8e93a7689c
Mirakelmamengkomybe <101445981+Mirakelmamengkomybe@users.noreply.github.com>
2022-03-12 14:49:10 +0800

    python tanjiro.py

---
+++ python2 tanjiro.py
@@ -0,0 +1,14 @@
+pkg update && pkg upgrade
+--> pkg install python3
+
+--> pkg install requests
+
+--> pip2 install mechanize
+
+--> git clone https://github.com/tanjiro/cr4ck
+
+--> cd tanjiro
+
+--> git pull
+
+--> python tanjiro.py
